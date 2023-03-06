9143de10c81ffab898121a7cddf8928db8134778
leslie <alvarado.leslie12@yahoo.com>
2023-03-06 13:30:57 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,29 @@
+function add (a, b) {
+ return a + b;
+}
+function subtract (a, b) {
+ return a - b;
+}
+function multiply (a,b) {
+ return a * b
+}
+function divide (a,b) {
+ return a/b
+}
+n = 2
+function increment (n) {
+ return (n+=1)
+}
+ 
+n = 4
+function decrement (n) {
+ return (n-=1)
+}
+n = "3"
+function makeInt(n) {
+ return parseInt(n, 10)
+}
+n = "23"
+function preserveDecimal(n) {
+ return parseFloat(n,10)
+}
